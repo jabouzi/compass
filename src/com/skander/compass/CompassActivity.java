3c2d536337696916a3801f11a5debe69096a5852
Skander Jabouzi <jabouzi@gmail.com>
2014-06-25 03:33:05 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/compass/trunk@9 01fa0fc7-abbf-4b69-96df-197fe14d50d0

--- src/com/skander/compass/CompassActivity.java
+++ src/com/skander/compass/CompassActivity.java
@@ -1,175 +1,87 @@
 package com.skander.compass;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import android.app.Activity;
 import android.content.Context;
+import android.widget.TextView;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
-import android.view.animation.TranslateAnimation;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.util.Log;
-import java.util.Arrays;
 
 public class CompassActivity extends Activity {
-		
-	private ImageView image1;
-	private ImageView image2;
-	private ImageView image3;	
+	float[] aValues = new float[3];
+	float[] mValues = new float[3];
+	float current_heading = 0.0f;
+	SensorManager sensorManager;
 	private TextView view1;
 	private TextView view2;
 	private TextView view3;
 	private TextView view4;	
-	private float[] aValues = new float[3];
-	private float[] mValues = new float[3];
-	private float orientation[] = new float[3];	
-	private float current_heading = 0f;
-	private float current_pitch = 0f;
-	private float current_roll = 0f;	
-	private float currentAcceleration = 0;
-	private float maxAcceleration = 0; 	
-	static final float ALPHA = 0.2f;
-	private SensorManager sensorManager;
+	static final float ALPHA = 0.25f;
 	
     /** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle icicle) {
 	  super.onCreate(icicle); 
-	  setContentView(R.layout.qibla);
-	  image1 = (ImageView) findViewById(R.id.compass1);
-	  image2 = (ImageView) findViewById(R.id.compass2);
-	  image3 = (ImageView) findViewById(R.id.compass3);
-	  
-	  view1 = (TextView) findViewById(R.id.view1);
+	  setContentView(R.layout.main);
+		view1 = (TextView) findViewById(R.id.view1);
 	  view2 = (TextView) findViewById(R.id.view2);
 	  view3 = (TextView) findViewById(R.id.view3);
 	  view4 = (TextView) findViewById(R.id.view4);
-	  rotate(image3, 0f, 58.64f, 0);
 	  sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-	  //updateOrientation();
-	  //Timer updateTimer = new Timer("gForceUpdate");
-		//updateTimer.scheduleAtFixedRate(new TimerTask() {
-		  //public void run() {
-			//updateOrientation();
-		  //}
-		//}, 0, 100);
-	  	//int loc[] = new int[2];
-		//image1.getLocationOnScreen(loc);
-		//Log.d("COMPASS : X", String.valueOf(loc[0]));
-		//Log.d("COMPASS : Y", String.valueOf(loc[1]));
-		//move(image1, 0, 5, 0, 5, 0);
-		//image1.getLocationOnScreen(loc);
-		//Log.d("COMPASS : X", String.valueOf(loc[0]));
-		//Log.d("COMPASS : Y", String.valueOf(loc[1]));
+	  updateOrientation(new float[] {0, 0, 0});
 	}
     
-    private void updateOrientation() {
-		//runOnUiThread(new Runnable() {
-			//public void run() {
-			
-				rotate(image2, current_heading, orientation[0], 100);		
-				current_heading = -orientation[0];
-				current_pitch = orientation[1];
-				current_roll = orientation[2];
-				view4.setText("ORIENTATION 4 : "+String.valueOf((int)orientation[0]));
-				//view2.setText("PITCH : "+Float.toString(values[1]));
-				//view3.setText("ROLL : "+Float.toString(values[2]));
-				//Log.d("COMPASS : HEADING2", String.valueOf(current_heading));
-			//}
-		//});
+    private void updateOrientation(float[] values) {
+		current_heading = normalizeDegree(values[0]);
+		view4.setText("ORIENTATION : "+String.valueOf(current_heading));
    	}
     
-    private void calculateOrientation() {
-		
-		float R[] = new float[9];
-		float I[] = new float[9];
-
-		view1.setText("ORIENTATION 1 : "+String.valueOf(mValues[0]));
-		boolean success = SensorManager.getRotationMatrix(R, I, aValues, mValues);
-		//SensorManager.remapCoordinateSystem(R, SensorManager.AXIS_X, SensorManager.AXIS_Z, R);
-		
-		if (success) {
-			SensorManager.getOrientation(R, orientation);
+    private float[] calculateOrientation() {
+      float[] values = new float[3];
+      float[] R = new float[9];
+      float[] outR = new float[9];
       
-		    // Convert from Radians to Degrees.
-			orientation[0] = (float) Math.toDegrees(orientation[0]);
-			view2.setText("ORIENTATION 2 : "+String.valueOf(orientation[0]));
-			orientation[0] = normalizeDegree(orientation[0]);
-			view3.setText("ORIENTATION 3 : "+String.valueOf(orientation[0]));
-			orientation[1] = (float) Math.toDegrees(orientation[1]);
-			orientation[2] = (float) Math.toDegrees(orientation[2]);
-			updateOrientation();
-		}
-		
-        //return orientation;
+      	//int rotation = Compatibility.getRotation(this);
+//
+		//if (rotation == 1) {
+			//SensorManager.remapCoordinateSystem(R, SensorManager.AXIS_X, SensorManager.AXIS_MINUS_Z, outR);
+		//} else {
+			//SensorManager.remapCoordinateSystem(R, SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_Z, outR);
+		//}
+//
+		//SensorManager.getOrientation(outR, values);
+
+      SensorManager.getRotationMatrix(R, null, aValues, mValues);
+      //SensorManager.remapCoordinateSystem(R, SensorManager.AXIS_X, SensorManager.AXIS_Z, outR);
+      //SensorManager.remapCoordinateSystem(R, SensorManager.AXIS_Y, SensorManager.AXIS_Z, outR);
+//
+      SensorManager.getOrientation(R, values);
+
+      // Convert from Radians to Degrees.
+      values[0] = (float) Math.toDegrees(values[0]);
+      values[1] = (float) Math.toDegrees(values[1]);
+      values[2] = (float) Math.toDegrees(values[2]);
+
+      return values;
     }
     
-	protected float[] lowPass(float[] input, float[] output)
-	{
-		if (output == null)	return input;
-
-		for (int i = 0; i < input.length; i++)
-		{
-			output[i] = output[i] + ALPHA * (input[i] - output[i]);
-			//output[i] = (input[i] * ALPHA) + (output[i] * (1.0f - ALPHA));
-		}
-		return output;
-	}
-	
-	private float normalizeDegree(float value){
-		  value = Math.round(value);
-          if(value >= 0.0f && value <= 180.0f){
-              return value;
-          }else{
-              return 180.0f + (180.0f + value);
-          }
-	}
-    
-    private void rotate(ImageView imgview, float current_degree,  float degree, int duration) {
-		RotateAnimation rotateAnim = new RotateAnimation(current_degree, -degree,
-		RotateAnimation.RELATIVE_TO_SELF, 0.5f,
-		RotateAnimation.RELATIVE_TO_SELF, 0.5f);
-
-		rotateAnim.setDuration(duration);
-		rotateAnim.setFillAfter(true);
-		imgview.startAnimation(rotateAnim);
-	}
-	
-    private void move(ImageView imgview, float x1,  float x2, float y1, float y2, int duration) {
-		TranslateAnimation translateAnim = new TranslateAnimation(TranslateAnimation.RELATIVE_TO_SELF, x1, TranslateAnimation.RELATIVE_TO_SELF, 
-		x2, TranslateAnimation.RELATIVE_TO_SELF, y1, TranslateAnimation.RELATIVE_TO_SELF, y2);
-		translateAnim.setDuration(duration);
-		translateAnim.setFillAfter(true);
-		imgview.startAnimation(translateAnim);
-	}
-	
     private final SensorEventListener sensorEventListener = new SensorEventListener() {
       public void onSensorChanged(SensorEvent event) {
-		synchronized (this)
-		{
-			if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) 
-				aValues = lowPass(event.values.clone(), aValues);
-				//aValues = event.values;
-			if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) 
-				mValues = lowPass(event.values.clone(), mValues);
-				//mValues = event.values;
-			if (aValues != null && mValues != null) {
-				calculateOrientation();
-			}
-		}
-     }
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+          //aValues = event.values;
+          aValues = lowPass(event.values.clone(), aValues);
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+          //mValues = event.values;
+          mValues = lowPass(event.values.clone(), mValues);
+
+        updateOrientation(calculateOrientation());
+      }
 
       public void onAccuracyChanged(Sensor sensor, int accuracy) {}
    	};
-
-
+   	
    	@Override
    	protected void onResume() {
    	  super.onResume();
@@ -190,4 +102,25 @@ public class CompassActivity extends Activity {
    	  sensorManager.unregisterListener(sensorEventListener);
    	  super.onStop();
    	}
+   	
+   	private float[] lowPass(float[] input, float[] output)
+	{
+		if (output == null)	return input;
+
+		for (int i = 0; i < input.length; i++)
+		{
+			output[i] = output[i] + ALPHA * (input[i] - output[i]);
+			//output[i] = (input[i] * ALPHA) + (output[i] * (1.0f - ALPHA));
+		}
+		return output;
+	}
+	
+	private float normalizeDegree(float value){
+		  value = Math.round(value);
+          if(value >= 0.0f && value <= 180.0f){
+              return value;
+          }else{
+              return 180.0f + (180.0f + value);
+          }
+	}
 }
